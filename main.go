f9224b27c0a127ad3533fc6a80f5a609f552ac0a
Haybat <oemer.haybat@it-designers.de>
2023-05-12 00:04:40 +0200

    sample http.ListenAndServe

--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+const hostUrl string = "localhost:9090"
+
 func main() {
 
+	http.HandleFunc("/hello", func(responseWriter http.ResponseWriter, request *http.Request) {
+		log.Println("Hello microservice.")
+
+		data, err := io.ReadAll(request.Body)
+		if err != nil {
+			http.Error(responseWriter, "Error in service", http.StatusBadRequest)
+			return
+		}
+
+		fmt.Fprintf(responseWriter, "Hello %s", data)
+	})
+
+	http.HandleFunc("/goodbye", func(responseWriter http.ResponseWriter, request *http.Request) {
+		log.Println("Goodbye go microservice.")
+	})
+
+	http.ListenAndServe(hostUrl, nil)
 }
